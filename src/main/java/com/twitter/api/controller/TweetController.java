779d1e353e330f0366debed0bd6e1113a578e5f0
Daniel Oliveira <danielcoliv98@gmail.com>
2023-01-21 18:14:51 -0300

    feature: get tweets from username

--- src/main/java/com/twitter/api/controller/TweetController.java
+++ src/main/java/com/twitter/api/controller/TweetController.java
@@ -3,6 +3,8 @@ package com.twitter.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,5 +30,10 @@ public class TweetController {
     User user = userRepository.findByUsernameIs(req.username()).get(0);
     TweetDTO tweet = new TweetDTO(req.username(), user.getAvatar(), req.tweet());
     tweetRepository.save(new Tweet(tweet));
+  }
+
+  @GetMapping("/{username}")
+  public List<Tweet> listAllTweets(@PathVariable String username) {
+    return tweetRepository.findByUsernameIs(username);
   }
 }
--- src/main/java/com/twitter/api/repository/TweetRepository.java
+++ src/main/java/com/twitter/api/repository/TweetRepository.java
@@ -1,9 +1,11 @@
 package com.twitter.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.twitter.api.model.Tweet;
 
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
-
+  List<Tweet> findByUsernameIs(String name);
 }
